b89c7067013ee0a2d869670683e7c28eea8e6f74
Denise Ly <dply95@gmail.com>
2017-05-02 03:32:14 -0400

    Created dict of ssid and bssid, run airodump

--- hax.py
+++ hax.py
@@ -5,6 +5,7 @@ from prettytable import PrettyTable
 from libnmap.parser import NmapParser
 import os
 import sys
+import time
 
 
 FILE_PREFIX = "results"
@@ -19,7 +20,7 @@ def clean_old_results():
         pass
 
     try:
-        os.remove(FILE_PREFIX + ".csv")
+        os.remove(FILE_PREFIX + "-01.csv")
     except FileNotFoundError:
         pass
 
@@ -35,24 +36,25 @@ def dump_network_info():
     # Wifi interface
     interface = subprocess.check_output(["nmcli", "-t", "-f", "DEVICE", "connection","show", "--active"]).decode().strip()
     # SSID of network user is connected to
-    sp1 = subprocess.Popen(["iw", "dev", "wlan0", "link"], stdout=subprocess.PIPE)
+    print("Scanning for wireless networks...")
+    sp1 = subprocess.Popen(["iw", "dev", interface, "link"], stdout=subprocess.PIPE)
     sp2 = subprocess.Popen(["grep", "SSID"], stdin=sp1.stdout, stdout=subprocess.PIPE)
     user_ssid = subprocess.check_output(["cut", "-f2-", "-d:"], stdin=sp2.stdout).decode().strip()
     # TODO: Note that iwgetid relies on deprecated iwconfig and does not work in modern Debian.
     #       Can use nmcli to determine the current network instead?
 
-    ssids = subprocess.Popen(["nmcli", "-t", "-f", "SSID", "dev", "wifi"], stdout=subprocess.PIPE).communicate()[0].decode()
-    # Remove duplicates
-    ssids_set = set(ssids.split('\n'))
-    # Create table
+    # Create a dictionary of ssids and bssids -- {ssid : bssid}
+    iw_scan1 = subprocess.Popen(["sudo", "iw", interface, "scan"], stdout=subprocess.PIPE)
+    iw_scan2 = subprocess.Popen(["egrep", "^BSS|SSID:"], stdin=iw_scan1.stdout, stdout=subprocess.PIPE).communicate()[0].decode().strip().splitlines()
+    ssid_dict = {}
+    for bssid, ssid in zip(iw_scan2[0::2], iw_scan2[1::2]):
+        ssid_dict[ssid[7:]] = bssid[4:21]
+    # Create enumerated table
     ssids_table = PrettyTable(["*","Option","SSID"])
-    # ssids_table.hrules=True
-    # ssids_table.vrules=False
-    # ssids_table.border=False
     option_num = 1
-    for entry in ssids_set:
+    for entry in ssid_dict:
         connected = ""
-        if entry == user_ssid.strip():
+        if entry == user_ssid:
             connected += "*"
         ssids_table.add_row([connected, option_num, entry])
         option_num+=1
@@ -60,32 +62,38 @@ def dump_network_info():
     print("* = You are connected to this network -- detailed results available\n")
     # TODO: Deal with bad input
     user_input = input("Choose an SSID to target: ")
-    network = ssids_table.get_string(border=False, header=False, fields=["SSID"], start=int(user_input)-1, end=int(user_input)).strip()
-    if network == user_ssid:
-        print("You are connected to " + network + ". Running nmap...", end="")
+    victim_network = ssids_table.get_string(border=False, header=False, fields=["SSID"], start=int(user_input)-1, end=int(user_input)).strip()
+    if victim_network == user_ssid:
+        print("You are connected to " + victim_network + ". Running nmap... ")
         # Get IPs
-        # TODO: use all interfaces that start with "wl"???
-        sp = subprocess.Popen(["ip", "addr", "show", "wlan0"], stdout=subprocess.PIPE)
+        sp = subprocess.Popen(["ip", "addr", "show", interface], stdout=subprocess.PIPE)
         ips = subprocess.check_output(["grep", "inet", "-m", "1"], stdin=sp.stdout).decode()[9:24]
         sp.wait()
-        subprocess.call(["nmap", "-n", "-A", "-oX", FILE_PREFIX+".xml", ips], stdout=open(os.devnull, 'wb')) # should we print nmap results to screen too?
+        # Not sure if nmap is working correctly anymore :|
+        nmap = subprocess.call(["nmap", "-n", "-A", "-oX", FILE_PREFIX+".xml", ips], stdout=subprocess.DEVNULL) # should we print nmap results to screen too?
         print("Done.\n")
     else:
-        print("You are not connected to " + network + ".\n")
+        print("You are not connected to " + victim_network + ".\n")
+        print("victim bssid: " + ssid_dict[victim_network])
 
     # Airodump -- working on this
-    # print("Enabling monitor mode... ", end="")
-    # try:
-    #     subprocess.check_output(["sudo", "airmon-ng", "start", interface])
-    #     print("Success")
-    #     airodump = subprocess.Popen(["sudo", "airodump-ng", interface+"mon", "-w", FILE_PREFIX, "-o", "csv"], stdout=subprocess.PIPE) #doesn't work
-    #     o_airodump, unused_stderr = airodump.communicate(timeout=10)
-    #     airodump.kill()
-    #     subprocess.check_output(["sudo", "airmon-ng", "stop", interface+"mon"])
-    #     subprocess.call(["service", "network-manager", "start"])
-    # except subprocess.CalledProcessError as e:
-    #     print ("Failed to enable monitor mode: " + e.output)
-
+    print("Enabling monitor mode... ")
+    try:
+        subprocess.check_output(["sudo", "airmon-ng", "start", interface])
+        print("Starting airodump for 10 seconds... ")
+        airodump = subprocess.Popen(["sudo", "airodump-ng","--bssid", ssid_dict[victim_network], interface+"mon", "-w", FILE_PREFIX, "-o", "csv"], stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+        # After 10 seconds, raise a TimeoutExpired exception to stop airodump
+        o_airodump, unused_stderr = airodump.communicate(timeout=10)
+        airodump.terminate()
+    except subprocess.CalledProcessError as e:
+        print ("Failed to enable monitor mode: " + e.output)
+    except subprocess.TimeoutExpired:
+        print("Disabling monitor mode... ")
+        subprocess.check_output(["sudo", "airmon-ng", "stop", interface+"mon"])
+        subprocess.call(["service", "network-manager", "start"])
+        print("Done.\n")
+        # Fixes invisible text in terminal & no echo after terminating airodump
+        subprocess.call(["stty", "sane"])
 
 def create_target_table():
     """
@@ -96,7 +104,7 @@ def create_target_table():
 
     # TODO: Parse results from dump_network_info
     try:
-        with open(FILE_PREFIX + ".csv") as airodump_file:
+        with open(FILE_PREFIX + "-01.csv") as airodump_file:
             i = 0
             for line in airodump_file:
                 print("line {}: {}".format(i, line.strip()))
@@ -131,7 +139,7 @@ if __name__ == "__main__":
 
     # TODO: Throw error if user is not using 'sudo'
     parse_arguments()
-    # clean_old_results()
+    clean_old_results()
     dump_network_info()
     create_target_table()
     print("uber l33t haxxing just happened")
